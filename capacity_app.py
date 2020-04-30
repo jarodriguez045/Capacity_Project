eb44a27db74af148c5a0eccfdb4f37d15acf9664
Jose_Ubuntu <jarodriguez045@gmail.com>
2020-04-29 21:48:27 -0400

    minor changes

--- capacity_app.py
+++ capacity_app.py
@@ -1,11 +1,10 @@
-import pandas
+import pandas as pd
 from pathlib import Path
 
 # modify file location before deployment
 workbook = Path("/home/jose_rodriguez/Desktop/github/Capacity_Project/workbooks/capacity_metrics2.csv")
 
 securesync = "SecureSync"
-data_tbl = pandas.read_csv(workbook, index_col='WK', parse_dates=['Date'], header=0)
+data_tbl = pd.read_csv(workbook, index_col='WK', parse_dates=['Date'], header=0)
 
 data_tbl['Actual Time'].where(data_tbl['Product #']==securesync)
-

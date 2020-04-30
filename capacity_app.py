76d1e97b55cdcf9112ed33a3d24116d936adbfb4
Jose_Ubuntu <jarodriguez045@gmail.com>
2020-04-29 23:00:15 -0400

    WE HAVE AVERAGES!

--- capacity_app.py
+++ capacity_app.py
@@ -1,10 +1,19 @@
 import pandas as pd
+import numpy as np
 from pathlib import Path
 
 # modify file location before deployment
 workbook = Path("/home/jose_rodriguez/Desktop/github/Capacity_Project/workbooks/capacity_metrics2.csv")
 
-securesync = "SecureSync"
-data_tbl = pd.read_csv(workbook, index_col='WK', parse_dates=['Date'], header=0)
+# edit to allow user input. 
+# sort csv to read line dataframe
+dt = pd.read_csv(workbook, index_col='WK', parse_dates=['Date'], header=0)
 
-data_tbl['Actual Time'].where(data_tbl['Product #']==securesync)
+# sort data by variable and create list of values
+is_ss = dt['Product Family']=='SecureSync'
+dt_pf = dt[is_ss]
+lst_atime = dt_pf['Actual Time'].to_list()
+
+# Calculate Average
+avg = np.nanmean(lst_atime)
+print(avg)
